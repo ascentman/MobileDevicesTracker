36d658f6924fb046f4eaeee92d9c242bc29b2079
Volodymyr Rykhva <volodymyr.rykhva@sigma.software>
2018-10-05 10:51:45 +0300

    initial commit

---
+++ MobileDevicesTracker/MobileDevicesTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MobileDevicesTracker
+//
+//  Created by Vova on 10/5/18.
+//  Copyright © 2018 Vova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
