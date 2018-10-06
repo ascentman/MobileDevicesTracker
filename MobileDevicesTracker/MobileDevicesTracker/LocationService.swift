e8531cdedd66ee0cc1891b80ce1d83d29faeaeb5
Kyryl <kirill.ge@gmail.com>
2018-10-06 10:03:30 +0300

    master > feedback and comments

--- MobileDevicesTracker/MobileDevicesTracker/LocationService.swift
+++ MobileDevicesTracker/MobileDevicesTracker/LocationService.swift
@@ -9,36 +9,69 @@
 import Foundation
 import CoreLocation
 
+// можна ж додати делегат і потім його обробити у контролері типу
+protocol LocationServiceDelegatel: class {
+    func didGetUserLocationUpdate(_ userLocation: CLLocation)
+    func didGetError(_ error: Error)
+
+}
+
 final class LocationService: NSObject {
+
+    weak var delegate: LocationServiceDelegate?
     
     static let shared = LocationService()
     private let locationManager = CLLocationManager()
     private var coordinates: CLLocationCoordinate2D?
+
+    // MARK: - LifeCycle
     
     private override init() {
         super.init()
+
         locationManager.delegate = self
+
         setupLocationManager(locationManager)
     }
+
+    // треба метод для запиту дозволу на локацію ОКРЕМО
+    // є метод делагут для отримання feedback
+    //    optional public func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus)
+
+    //  якшо прямо так як ти робиш в init - тоді ти не контролюєш цей момент взагалі і запит відразу виконується як створюєш обєкт, а що робити коли треба по натисканню кнопки показати попап - з такою логікою не вийде
+    // короч окремий метод з completion
     
+
     func requestLocation() -> CLLocationCoordinate2D {
         locationManager.requestLocation()
         if let coordinates = coordinates {
             return coordinates
         }
+        // а тут і причина чого ти 0,0 отримуєш - бо coordinates nil адже треба певний час на отримання локаціїї
         return CLLocationCoordinate2D(latitude: 0, longitude: 0)
     }
     
     // MARK: - Private
     
     private func setupLocationManager(_ locationManager: CLLocationManager) {
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest // нам дійсно треба best? чи для статистики може вистачити kCLLocationAccuracyHundredMeters
+
+//        locationManager.distanceFilter // також треба для оптимізації
         locationManager.requestAlwaysAuthorization()
-        locationManager.startUpdatingLocation()
+        locationManager.startUpdatingLocation() // треба робити тільки після того як ти вже впевнений шо достпу до локації наданий - інакше воно не робитиме
+
+//CLLocationManager.authorizationStatus() - поверне наявний статус і якшо він дозволений то не треба питати доступ
+
+    // в тебе deployment target 12 - нащо перевіряти це - просто тупо копі-паст - так треба ж думати нашо коден рядок....
         if #available(iOS 9, *) {
             locationManager.allowsBackgroundLocationUpdates = true
         }
     }
+
+    // privacy field in plist - то шоб наверняка - обидва варіанти, можливо один включає інший і обидва варіанти не потрібні - чи тут знову підхід - "робить - не чіпати, розбиратися не треба"
+    //я ж кинув документацію - там все конкртено описано....
+    // засмутив перевіряючого ти :(
+
 }
 
 // MARK: - Extensions
--- MobileDevicesTracker/MobileDevicesTracker/ViewController.swift
+++ MobileDevicesTracker/MobileDevicesTracker/ViewController.swift
@@ -9,25 +9,55 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
+// назва - default треба замінити
+class ViewController: UIViewController { //final
 
-    @IBOutlet weak var map: MKMapView!
-    
+    @IBOutlet weak var map: MKMapView! //private
+
+    // тут краще мати обєкт а не обєкт і відразу запит
+    // тобі треба запит зробити на viewDidLoad і дотого ж треба отримати результ того що користувач вибрав - completion(_ accessGranted: Bool, error: Error?) або шось таке
     private var currentLocation = LocationService.shared.requestLocation()
+
+    // MARK: - LifeCYcle
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let device = MobileDevice()
+
+        /*
+
+         "Device type" : "",
+         "Workplace" : "",
+         "Inventory number" : "",
+         "Project" : "",
+         "Sticker" : "",
+         "Department/Project device" : "",
+         "Responsible person" : ""
+
+         для цих полів треба додати буде введення інформації вручну з таблички наприклад з статичниси целами
+ */
+
         let dict = device.getMobileDeviceInfo()
         print(dict)
     }
+
+    // MARK: - IBActions
     
-    @IBAction func clicked(_ sender: Any) {
-        
+    @IBAction func clicked(_ sender: Any) { //private
+
+        // нуль бо в тебе ще нічого не почалося і це default значення яке ти сам і вказав
+        //        return CLLocationCoordinate2D(latitude: 0, longitude: 0)
+        //ти ж час пишеш код і те шо ти вказав те і отримаєш
+        // + можна ж продебажити і глянути чого тут 0,0
         print(currentLocation.latitude, currentLocation.longitude) // first time always (0,0). Need some observer or I don't know(
         self.currentLocation = LocationService.shared.requestLocation()
-        let span: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: 0.01,longitudeDelta: 0.01)
-        let region: MKCoordinateRegion = MKCoordinateRegion(center: currentLocation, span: span)
+
+        //хіба була вимога показувати карту? чи вимога була отримати координати періодично в bg?
+        // не треба нічого додаткового - принаймні на першому кроці - бо зараз те шо треба - не робить як треба і ще й щось є чого не треба
+        // :(
+        let span: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: 0.01,longitudeDelta: 0.01) // нашо це?
+        let region: MKCoordinateRegion = MKCoordinateRegion(center: currentLocation, span: span)  // нашо це?
         map.setRegion(region, animated: true)
         self.map.showsUserLocation = true
     }
